767809d5f6e9930b1365b5dd1edeb703870e083d
Harjinder Singh Banga <hsbanga@yahoo.com>
2021-06-13 21:47:46 +0530

    Add files via upload

---
+++ target/cucumber-parallel-report/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "c7e41b58-0cab-4ef4-8296-b00cce03fea9",
+    "feature": "Verify the wishlist functionality",
+    "scenario": "PQ-TC-2144 - Verify the wishlist functionality",
+    "start": 1623600249077,
+    "group": 19,
+    "content": "",
+    "tags": "@tag1,@westwingnow,",
+    "end": 1623600306605,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  }
+]);
+});
